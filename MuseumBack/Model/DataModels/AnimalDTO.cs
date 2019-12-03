18e905ade5213fed6075c054753d462c05b8c691
stef beys <stef.beys@hotmail.nl>
2019-12-03 12:50:51 +0200

    Added Additional properties

--- MuseumBack/Model/DataModels/AnimalDTO.cs
+++ MuseumBack/Model/DataModels/AnimalDTO.cs
@@ -7,5 +7,9 @@
         public string Diet { get; set; }
         public string Behaviour { get; set; }
         public string Endangerment { get; set; }
+        public string Length { get; set; }
+        public string Width { get; set; }
+        public string DietShort { get; set; }
+        public string Region { get; set; }
     }
 }
